49e626e399e59b2ea78c4bb8aaa22dbee35df717
CodeBridgeGenerator <kana@codebridge.my>
2024-08-05 02:46:24 +0000

    CodeBridge commit

---
+++ nodejs-backend/src/services/userGuide/userGuide.service.js
@@ -0,0 +1,20 @@
+const { UserGuide } = require('./userGuide.class');
+const createModel = require('../../models/userGuide.model');
+const hooks = require('./userGuide.hooks');
+
+module.exports = function (app) {
+  const options = {
+    Model: createModel(app),
+    paginate: app.get('paginate'),
+    whitelist: ["$populate"],
+    multi: ["create"],
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/userGuide', new UserGuide(options, app));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service('userGuide');
+
+  service.hooks(hooks);
+};
